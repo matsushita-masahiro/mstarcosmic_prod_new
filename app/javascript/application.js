86f2b396220bd6de0d5a1d3057f2caa94cb161d0
Ubuntu <ubuntu@ip-172-31-37-22.ap-southeast-1.compute.internal>
2025-04-03 18:26:05 +0000

    pg update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+console.log("JavaScript is working!");
+$(document).ready(function() { console.log("jQuery is working!"); });
